5bc4a6a8249ea4f6c553ac3ddfea68878ac10d23
TrungDuc991 <dotrungduc.bl@gmail.com>
2023-10-26 00:22:29 +0700

    fix upload files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const multer = require("multer");
 const logger = require("morgan");
 const serveIndex = require("serve-index");
 require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -36,19 +37,38 @@ app.use(
 );
 
 app.get("/", function (req, res) {
-  return res.send("hello from my app express server!");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/UploadFile", upload.single("file"), function (req, res) {
+app.post("/uploadfile", upload.single("file"), function (req, res) {
   debug(req.file);
-  console.log("storage location is ", req.hostname + "/" + req.file.path);
-  return res.send(req.file);
+
+  return res.send({
+    ...req.file,
+    url: `http://localhost:${port}/ftp/uploads/${req.file.filename}`,
+  });
+});
+
+app.post("/uploadmultiple", upload.array("files", 12), (req, res, next) => {
+  debug(req.files);
+  const files = req.files;
+  let newFiles = files.map((file) => {
+    return {
+      ...file,
+      url: `http://localhost:${port}/ftp/uploads/${file.filename}`,
+    };
+  });
+  if (!files) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.send(newFiles);
 });
 
 //if end point is /users/, use the router.
 app.use("/users", userRouter);
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   debug("Server is up and running on port ", port);
   console.log(`Server is running at port http://localhost:${port}`);
